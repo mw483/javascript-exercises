cb2921162e9a954c0c260525f4a70d39ca74a452
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-03-31 11:43:58 +0900

    solution for 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, current) => sum + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+  let factorial = 1;
+  while (a > 0) {
+      factorial = factorial * a;
+      a--
+    }
+  return factorial
+  };
 
 // Do not edit below this line
 module.exports = {
