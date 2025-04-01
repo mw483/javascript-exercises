7191d383b19ffa74099326ab50af5f24341ea7b5
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-04-01 21:38:33 +0900

    my own solution works

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+let string = "racecar";
 
-};
+const palindromes = function (string) {
+    let stringArray = string.toUpperCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').replace(/\s+/g, '').split('');
+    let stringArrayCopy = [];
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+      stringArrayCopy.push(stringArray[i])
+    }
+    if (stringArray.toString() === stringArrayCopy.toString()) {
+      return true
+    }
+    else return false};
+
+palindromes(string)
 
 // Do not edit below this line
 module.exports = palindromes;
