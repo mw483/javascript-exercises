749a9b782f6bc9557852f79f910e5bd288ab662f
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-04-02 22:04:58 +0900

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(number) {
+    let fibonacciNo;
+    if (Number(number) === 0) {
+        fibonacciNo = 0;
+    }
+    else if (Number(number) === 1) {
+        fibonacciNo = 1;
+    }
+    else if (Number(number) > 1) {
+        fibonacciNo = fibonacci(Number(number) - 1) + fibonacci (Number(number) - 2) 
+        }
+    else if (Number(number) < 0) {
+      fibonacciNo = "OOPS"
+    }
+    return fibonacciNo
+    };
 
 // Do not edit below this line
 module.exports = fibonacci;
