8997d7a182f29f7d17838035a9d72c28826c3c18
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-04-06 16:28:22 +0900

    12 return oldest person solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+const findTheOldest = function(array) {
+    let peopleWithAge = array.map()
+    
 };
 
 // Do not edit below this line
