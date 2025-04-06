115ac6f885648b96e0e3375e5495018adcfe7774
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-04-06 15:55:13 +0900

    11 getthetitles solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,19 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+
+
+const getTheTitles = function(books) {
+    let bookTitles = books.map(book => book.title)
+    return bookTitles
 };
 
 // Do not edit below this line
